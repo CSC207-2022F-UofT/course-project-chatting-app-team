35f3e4e9b36a79d96900117101ca8345e23c14da
Daiiii99 <113395523+Daiiii99@users.noreply.github.com>
2022-11-03 19:13:59 -0700

    Database2 (#13)
    * Post and reply
    
    * Update database_usage_example.java
    
    * done

--- src/main/java/database_connection/database_usage_example.java
+++ src/main/java/database_connection/database_usage_example.java
@@ -1,7 +1,26 @@
 package database_connection;
 
+import java.util.UUID;
+import java.time.Instant;
+
 import org.bson.Document;
+import static com.mongodb.client.model.Filters.eq;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import org.bson.types.ObjectId;
+
+
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Sorts.descending;
 
 public class database_usage_example {
 
@@ -9,15 +28,18 @@ public class database_usage_example {
         // paste in connection string
         Database myDatabase = new Database("<connection string goes here>", "DatingAppStaging");
 
-        // Use this function to insert a document into database.
-        myDatabase.insert_user (new Document()
-                .append("_id", new ObjectId())
-                .append("user_id", "a_new_user_id")
-                .append("your_own_property", "whatever value"));
+        //test for insert_post
+        myDatabase.insert_post("input_username", "input_post_content");
 
-        // user this to query for a user.
-        Document doc = myDatabase.find_user_by_id("my_user_name_test");
+        //test for insert_reply
+        myDatabase.insert_reply("_id","input_username", "input_reply_content");
 
-        System.out.println(doc);
+        //test for find_post_by_id
+        Document doc1 = myDatabase.find_post_by_id("_id");
+        System.out.println(doc1);
+
+        //test for find_latest_posts
+        List<Document> doc2 = myDatabase.find_latest_posts(1);
+        System.out.println(doc2);
     }
 }
