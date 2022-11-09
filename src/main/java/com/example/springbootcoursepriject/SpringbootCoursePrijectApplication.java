38affa97a09d10075e714e6db448273e91febc10
mugengliu <71624274+mugengliu@users.noreply.github.com>
2022-11-09 09:36:41 +0800

    hello, this is springboot

---
+++ src/main/java/com/example/springbootcoursepriject/SpringbootCoursePrijectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcoursepriject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCoursePrijectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCoursePrijectApplication.class, args);
+    }
+
+}
