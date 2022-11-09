55c22fe197dd927a8f1c0188696aabdf533ed002
mugengliu <71624274+mugengliu@users.noreply.github.com>
2022-11-09 09:36:41 +0800

    hello, this is springboot

---
+++ src/test/java/com/example/springbootcoursepriject/SpringbootCoursePrijectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcoursepriject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCoursePrijectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
