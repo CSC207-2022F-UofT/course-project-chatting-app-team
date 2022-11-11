1ad6534b47a913000917a05b44eefc967f3c8c1f
mugengliu <71624274+mugengliu@users.noreply.github.com>
2022-11-12 02:11:09 +0800

    1. Use Vue to realize the post history display 2. Use Vue to realize the displaying new post after clicking the sending message 3. Delete most part of the jQuery code 4. Add more comments, arrange all the click event at one place, making code more readable 5. Add servlet05, servlet06, one for receive the new message, one for response the new message from the frontend.

---
+++ src/main/java/com/xxxx/example/servlet05.java
@@ -0,0 +1,24 @@
+package com.xxxx.example;
+
+import database_connection.Database;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ser05")
+public class servlet05 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String get = req.getParameter("text");
+        String userName = req.getParameter("userName");
+
+        Database myDatabase = new Database("<connection string goes here>", "DatingAppStaging");
+        myDatabase.insert_post(userName, get);
+        req.getRequestDispatcher("/ser06").forward(req,resp);
+    }
+}
