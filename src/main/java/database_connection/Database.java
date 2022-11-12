c336385b0d1d269d0e682849359c2015d37136fb
Daiiii99 <113395523+Daiiii99@users.noreply.github.com>
2022-11-12 14:41:07 +0800

    like and unlike

--- src/main/java/database_connection/Database.java
+++ src/main/java/database_connection/Database.java
@@ -22,6 +22,11 @@ import java.util.Arrays;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Sorts.descending;
 
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.conversions.Bson;
+import com.mongodb.client.model.Updates;
+
 public class Database {
 
     String connection_uri = "";
@@ -91,5 +96,63 @@ public class Database {
 
     public List<Document> find_latest_posts(int num) {
         return find_latest_posts(num, 0);
+    }
+
+    public Document find_user_by_id(String user_id){
+
+        // Maybe this can be moved to an instance variable, need double-checking
+        MongoCollection<Document> user_collection = mongo_database.getCollection("user");
+        Document doc = user_collection.find(eq("user_id", user_id)).first();
+        return doc;
+
+    };
+
+    public void insert_user(Document doc) {
+        MongoCollection<Document> user_collection = mongo_database.getCollection("user");
+        user_collection.insertOne(doc);
+    }
+
+    public List<Document> find_latest_posts_by_user_id(int num, int offset, String user_id) {
+        MongoCollection<Document> post_collection = mongo_database.getCollection("post");
+
+        List<Document> docs = new ArrayList<>();
+        post_collection.aggregate(
+                Arrays.asList(
+                        Aggregates.match(Filters.eq("user_nickname", user_id)),
+                        Aggregates.lookup("reply", "_id", "parent_post_id", "replies"),
+                        Aggregates.sort(descending("created_on")),
+                        Aggregates.skip(offset),
+                        Aggregates.limit(num)
+                )
+        ).into(docs);
+
+        return docs;
+    }
+
+    public List<Document> find_latest_posts_by_user_id(int num, String user_id) {
+        return find_latest_posts_by_user_id(num, 0, user_id);
+    }
+
+    public void like(String user_id, String post_id){
+        MongoCollection<Document> post_collection = mongo_database.getCollection("post");
+
+        // query to filter the post using post id
+        Document query = new Document().append("_id",  post_id);
+        Bson updates = Updates.addToSet("likes", user_id);
+        UpdateOptions options = new UpdateOptions().upsert(true);
+        UpdateResult result = post_collection.updateOne(query, updates, options);
+
+        // Optional
+        // return Result
+    }
+
+    public void unlike(String user_id, String post_id){
+        MongoCollection<Document> post_collection = mongo_database.getCollection("post");
+
+        Document query = new Document().append("_id",  post_id);
+        Bson updates = Updates.pull("likes", user_id);
+        UpdateOptions options = new UpdateOptions().upsert(true);
+        UpdateResult result = post_collection.updateOne(query, updates, options);
+
     }
 }
