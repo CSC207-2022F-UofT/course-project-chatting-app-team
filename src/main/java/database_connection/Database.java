a672aa1976b871eefc04e971b4b7a853dd1ed632
Daiiii99 <113395523+Daiiii99@users.noreply.github.com>
2022-11-13 09:57:55 +0800

    Delete done

--- src/main/java/database_connection/Database.java
+++ src/main/java/database_connection/Database.java
@@ -1,31 +1,23 @@
 package database_connection;
 
-import java.util.UUID;
-import java.time.Instant;
-
+import com.mongodb.client.*;
 import org.bson.Document;
 import static com.mongodb.client.model.Filters.eq;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.types.ObjectId;
-
-
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
-import java.util.List;
-import java.util.ArrayList;
-
-import java.util.Arrays;
-import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Sorts.descending;
 
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.result.UpdateResult;
 import org.bson.conversions.Bson;
 import com.mongodb.client.model.Updates;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 public class Database {
 
@@ -55,6 +47,7 @@ public class Database {
     public void insert_reply(String parent_post_id, String user_nickname, String reply_content){
         MongoCollection<Document> reply_collection = mongo_database.getCollection("reply");
         String uuid = UUID.randomUUID().toString();
+
         Document reply = new Document();
         String UTC_time = Instant.now().toString();
         reply.append("_id", uuid)
@@ -153,6 +146,17 @@ public class Database {
         Bson updates = Updates.pull("likes", user_id);
         UpdateOptions options = new UpdateOptions().upsert(true);
         UpdateResult result = post_collection.updateOne(query, updates, options);
+
+    }
+
+    public void delete_post_by_id(String post_id) {
+
+        MongoCollection<Document> post_collection = mongo_database.getCollection("post");
+        MongoCollection<Document> reply_collection = mongo_database.getCollection("reply");
+
+        // post_id should be unique inside post collection, using deleteOne
+        post_collection.deleteOne(Filters.eq("_id", post_id));
+        reply_collection.deleteMany(Filters.eq("parent_post_id", post_id));
 
     }
 }
