4b53b21e88f34af46711639b0b7200aea9d2f901
ruoxinhe <ruoxin.he@mail.utoronto.ca>
2022-11-14 18:34:48 -0500

    Revert "Revert "added pop up windows and changed 'like' related functions""
    This reverts commit 51f47598fb5233ca760d590832471070def5c1a0.

--- src/main/java/com/xxxx/example/servletLiked.java
+++ src/main/java/com/xxxx/example/servletLiked.java
@@ -1,16 +1,23 @@
 package com.xxxx.example;
 
+import database_connection.Database;
+import org.bson.Document;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/listenLiked")
 public class servletLiked extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        resp.setCharacterEncoding("UTF-8");
+        Database myDatabase = new Database("mongodb+srv://K125_member2:asdf8765@cluster0.gnzfm1q.mongodb.net/test", "DatingAppStaging");
+//        List<Document> doc2 = myDatabase.get_liked_count
+//       post liked count
     }
 }
